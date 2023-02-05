da92a2b73a6ea86fb305fae782905f6b0faff14f
Nachiappan <nachiappann@microsoft.com>
2023-02-05 23:05:10 +0530

    Starting point for refactoring

--- Excercise001/TennisScoringApp/TennisScoringApp/TennisScorer.cs
+++ Excercise001/TennisScoringApp/TennisScoringApp/TennisScorer.cs
@@ -21,7 +21,7 @@ namespace TennisScoringApp
 
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
+            if (playerName == player1Name)
                 m_score1 += 1;
             else
                 m_score2 += 1;
@@ -53,10 +53,10 @@ namespace TennisScoringApp
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
                 var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                if (minusResult == 1) score = $"Advantage to {player1Name}";
+                else if (minusResult == -1) score = $"Advantage to {player2Name}";
+                else if (minusResult >= 2) score = $"Win for {player1Name}";
+                else score = $"Win for {player2Name}";
             }
             else
             {
